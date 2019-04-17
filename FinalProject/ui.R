0e4dd535deb4c757466fe758b9eb6b92a52f775e
Landon Hess <hessland21@gmail.com>
2019-04-17 20:42:57 +0000

    Initial shiny UI setup

--- FinalProject/ui.R
+++ FinalProject/ui.R
@@ -6,19 +6,48 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("The Perfect Fit"),
 
-    # Sidebar with a slider input for number of bins
+    # Sidebar layout
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
+        # Main panel will be the map of the United States. The plot will be made on the server side with ggplot2.
         mainPanel(
-            plotOutput("distPlot")
+            textOutput("this is where the plot goes")
+        ),
+        
+        # The sidebar panel will have all of the information as listed in the project proposal.
+        sidebarPanel(
+            # Our sidebar has two tabs, search, and details.
+            tabsetPanel(
+                
+                # First tab, search bar
+                tabPanel("Search",
+                         verticalLayout(
+                             h4("Search Parameters"),
+                             sliderInput("temp",
+                                         "Average Temperature:",
+                                         min = 0,
+                                         max = 80,
+                                         value = c(20,60)),
+                             sliderInput("crime",
+                                         "Max Crime Rate:",
+                                         min = 0,
+                                         max = 10,
+                                         value = c(0,5)),
+                             sliderInput("popdens",
+                                         "Population Density:",
+                                         min = 1,
+                                         max = 50,
+                                         value = c(10,30)),
+                             submitButton("Search")
+                         )
+                ), # End tab 1
+                
+                # Second tab, details
+                tabPanel("Details",
+                         verticalLayout(
+                             h4("Selected State")
+                         )
+                )
+            )
         )
     )
 ))
