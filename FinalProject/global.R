a24ad1346871bff55347c3f38c5b79c1a1304dcf
Landon Hess <hessland21@gmail.com>
2019-04-19 16:36:37 +0000

    Add JS reactive implementation to states, removed search button (event conflicts), cleaned up leaflet

--- FinalProject/global.R
+++ FinalProject/global.R
@@ -3,7 +3,6 @@ print("Starting global.R script.")
 library(shiny)
 
 # This is where packages are loaded into the global environment.
-library(usmap)
 library(ggplot2)
 library(leaflet)
 library(rvest)
@@ -11,5 +10,7 @@ library(geojsonio)
 
 # This is where we should do scraping. Load data into global variables.
 states = geojson_read("https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json", what = "sp")
+
+selected_state = NULL
 
 print("Global script ran successfully.")
--- FinalProject/server.R
+++ FinalProject/server.R
@@ -1,13 +1,36 @@
 # Define server logic
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  
   
   # Create the map with global 'states' variable.
-  g = leaflet(states) %>%
-    # Center the view on the continental 48 states.
-    setView(-96, 37.8, 4, zoom = 4.2) %>%
-    # Add the state outlines. 'weight' changes outline thickness.
-    addPolygons(weight = 1)
+  output$map <- renderLeaflet({
+    leaflet(states) %>%
+      # Center the view on the continental 48 states.
+      setView(-96, 37.8, 4, zoom = 4.2) %>%
+      # Add the state outlines. JavaScript reactive implementation here too.
+      addPolygons(
+        layerId = states$name,
+        weight = 1,
+        dashArray = 2,
+        fillColor = "green",
+        color = "white",
+        highlight = highlightOptions(
+          weight = 2,
+          opacity = 1,
+          bringToFront = TRUE,
+          dashArray = ""
+        )
+        
+      )
+  }) # End render leaflet.
   
-  # Set the output.
-  output$map = renderLeaflet(g)
+  # Listen for click on a state.
+  observeEvent(input$map_shape_click, {
+    click <- input$map_shape_click
+    selected_state = click$id
+    output$sel_state_name = renderText(click$id)
+    
+    # Switch to details tab.
+    updateTabsetPanel(session, inputId = "tabs", selected = "detailsTab")
+  })
 })
